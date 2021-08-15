fa5a049c0ae69cccf1851969919497cf8584e21a
trestist <51286905+TRESTIST@users.noreply.github.com>
2021-08-15 19:38:44 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,650 @@
+// database questions
+const quizDataAll = {
+  english: [
+    {
+      language: "english",
+      id: 0,
+      tag: "beginner",
+      name: "Beginner",
+      quiz: [
+        {
+          question: "1 How do you do?",
+          a: "SDASDAS",
+          b: "ADSASD",
+          c: "DSAD",
+          d: "ALLL",
+          correct: "d",
+        },
+        {
+          question: "1 Simple dimpl or Pop it?",
+          a: "Simple dimpl",
+          b: "no",
+          c: "Pop it",
+          d: "sckvirt",
+          correct: "d",
+        },
+        {
+          question: "1 What is JS?",
+          a: "JS",
+          b: "ZALUPA",
+          c: "Programming language",
+          d: "Iron man",
+          correct: "d",
+        },
+      ],
+      quiz2: [
+        {
+          question: "2 Input Antonyo",
+          word: "Ugly",
+          correct: "cool",
+        },
+        {
+          question: "2 Input Antonyo",
+          word: "You",
+          correct: "me",
+        },
+        {
+          question: "2 Input Antonyo",
+          word: "No",
+          correct: "yes",
+        },
+      ],
+      quiz3: [
+        {
+          question: "3 Comlite this ____",
+          first: "Hello my",
+          second: "Jon!",
+          correct: "name is",
+        },
+        {
+          question: "3 Comlite this ____",
+          first: "Have you",
+          second: "in mexico?",
+          correct: "ever been",
+        },
+        {
+          question: "3 Comlite this ____",
+          first: "Would you",
+          second: "some coffe?",
+          correct: "to drink",
+        },
+      ],
+    },
+    {
+      language: "english",
+      id: 1,
+      tag: "preIntermediate",
+      name: "Pre-Intermediate",
+      quiz: [
+        {
+          question: "1 How do you do?",
+          a: "SDASDAS",
+          b: "ADSASD",
+          c: "DSAD",
+          d: "ALLL",
+          correct: "d",
+        },
+        {
+          question: "1 Simple dimpl or Pop it?",
+          a: "Simple dimpl",
+          b: "no",
+          c: "Pop it",
+          d: "sckvirt",
+          correct: "d",
+        },
+        {
+          question: "1 What is JS?",
+          a: "JS",
+          b: "ZALUPA",
+          c: "Programming language",
+          d: "Iron man",
+          correct: "d",
+        },
+      ],
+      quiz2: [
+        {
+          question: "2 Input Antonyo",
+          word: "Ugly",
+          correct: "cool",
+        },
+        {
+          question: "2 Input Antonyo",
+          word: "You",
+          correct: "me",
+        },
+        {
+          question: "2 Input Antonyo",
+          word: "No",
+          correct: "yes",
+        },
+      ],
+      quiz3: [
+        {
+          question: "3 Comlite this ____",
+          first: "Hello my",
+          second: "Jon!",
+          correct: "name is",
+        },
+        {
+          question: "3 Comlite this ____",
+          first: "Have you",
+          second: "in mexico?",
+          correct: "ever been",
+        },
+        {
+          question: "3 Comlite this ____",
+          first: "Would you",
+          second: "some coffe?",
+          correct: "to drink",
+        },
+      ],
+    },
+    {
+      language: "english",
+      id: 2,
+      tag: "advanced",
+      name: "Advanced",
+      quiz: [
+        {
+          question: "1 How do you do?",
+          a: "SDASDAS",
+          b: "ADSASD",
+          c: "DSAD",
+          d: "ALLL",
+          correct: "d",
+        },
+        {
+          question: "1 Simple dimpl or Pop it?",
+          a: "Simple dimpl",
+          b: "no",
+          c: "Pop it",
+          d: "sckvirt",
+          correct: "d",
+        },
+        {
+          question: "1 What is JS?",
+          a: "JS",
+          b: "ZALUPA",
+          c: "Programming language",
+          d: "Iron man",
+          correct: "d",
+        },
+      ],
+      quiz2: [
+        {
+          question: "2 Input Antonyo",
+          word: "Ugly",
+          correct: "cool",
+        },
+        {
+          question: "2 Input Antonyo",
+          word: "You",
+          correct: "me",
+        },
+        {
+          question: "2 Input Antonyo",
+          word: "No",
+          correct: "yes",
+        },
+      ],
+      quiz3: [
+        {
+          question: "3 Comlite this ____",
+          first: "Hello my",
+          second: "Jon!",
+          correct: "name is",
+        },
+        {
+          question: "3 Comlite this ____",
+          first: "Have you",
+          second: "in mexico?",
+          correct: "ever been",
+        },
+        {
+          question: "3 Comlite this ____",
+          first: "Would you",
+          second: "some coffe?",
+          correct: "to drink",
+        },
+      ],
+    },
+  ],
+  german: [
+    {
+      language: "german",
+      id: 0,
+      tag: "beginner",
+      name: "1Beginner",
+      quiz: [
+        {
+          question: "How do you do?",
+          a: "SDASDAS",
+          b: "ADSASD",
+          c: "DSAD",
+          d: "ALLL",
+          correct: "d",
+        },
+        {
+          question: "Simple dimpl or Pop it?",
+          a: "Simple dimpl",
+          b: "no",
+          c: "Pop it",
+          d: "sckvirt",
+          correct: "d",
+        },
+        {
+          question: "What is JS?",
+          a: "JS",
+          b: "ZALUPA",
+          c: "Programming language",
+          d: "Iron man",
+          correct: "d",
+        },
+      ],
+      quiz2: [
+        {
+          question: "Input Antonyo",
+          word: "Ugly",
+          correct: "cool",
+        },
+        {
+          question: "Input Antonyo",
+          word: "You",
+          correct: "me",
+        },
+        {
+          question: "Input Antonyo",
+          word: "No",
+          correct: "yes",
+        },
+      ],
+    },
+    {
+      language: "german",
+      id: 1,
+      tag: "preIntermediate",
+      name: "1Pre-Intermediate",
+      quiz: [
+        {
+          question: "How do you do?",
+          a: "How are you?",
+          b: "How you do?",
+          c: "How do you do?",
+          d: "Салам",
+          correct: "d",
+        },
+        {
+          question: "Simple dimpl or Pop it?",
+          a: "Simple dimpl",
+          b: "no",
+          c: "Pop it",
+          d: "sckvirt",
+          correct: "d",
+        },
+        {
+          question: "What is JS?",
+          a: "JS",
+          b: "ZALUPA",
+          c: "Programming language",
+          d: "Iron man",
+          correct: "d",
+        },
+      ],
+      quiz2: [
+        {
+          question: "Input Antonyo",
+          word: "Ugly",
+          correct: "cool",
+        },
+        {
+          question: "Input Antonyo",
+          word: "You",
+          correct: "me",
+        },
+        {
+          question: "Input Antonyo",
+          word: "No",
+          correct: "yes",
+        },
+      ],
+    },
+  ],
+};
+
+// DOM Selectors
+// const english = document.querySelector("#english");
+// const german = document.querySelector("#german");
+// const math = document.querySelector("#math");
+const nav = document.querySelector(".nav");
+const sideNav = document.querySelector(".sidenav");
+const sideNavEls = document.querySelectorAll(".sidenav p");
+const mainHead = document.querySelector(".main");
+const main = document.querySelector(".main__inner");
+const quiz = document.querySelector(".quiz-container");
+const submitBtn = document.querySelector("#btn-next");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.querySelectorAll("#question");
+const a_text = document.querySelector("#a_text");
+const b_text = document.querySelector("#b_text");
+const c_text = document.querySelector("#c_text");
+const d_text = document.querySelector("#d_text");
+const numOfQuestions = document.querySelectorAll("#number-of-question");
+const numOfAllQuestions = document.querySelectorAll("#number-of-all-questions");
+
+const quizInput = document.querySelector(".input-quiz");
+const submitBtnInput = document.querySelector("#btn-next-quiz");
+const input = document.querySelector(".input-answer");
+const inputText = document.querySelector("#input-text");
+
+const quizComplite = document.querySelector(".quiz-complite");
+const compliteInput = document.querySelector(".complite-answer");
+const submitBtnComplite = document.querySelector("#btn-next-complite");
+const inputTextFirst = document.querySelector("#input-text-first");
+const inputTextSecond = document.querySelector("#input-text-second");
+
+window.addEventListener("load", () => {
+  let lastChose = localStorage.getItem("choseed");
+
+  const chose = quizDataAll[lastChose];
+  const temp = chose
+    .map((item, index) => {
+      CHOSEED = item.language;
+      return `
+    <div>
+    <h2>${item.name}</h2>
+    <div class="button">
+        <button onclick="openTest(${[index]})" class="${item.tag} ${
+        item.language
+      }" id="btn-next">Пройти</button>
+      </div>
+    </div>
+    `;
+    })
+    .join("");
+
+  main.innerHTML = temp;
+});
+sideNavEls.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    chosedQuiz = e.target.id;
+    if (localStorage.getItem("choseed")) {
+      localStorage.setItem("choseed", chosedQuiz);
+    } else {
+      localStorage.setItem("choseed");
+    }
+    reset();
+  });
+});
+
+//SOMEDATA
+let CHOSEED;
+let i = 0;
+let choseedQuiz;
+// score of right questions
+let score = 0;
+// used for if();
+let currentQuiz = 0;
+// count of question right now
+let questionNum = 1;
+
+// our answers
+let myAnswers = [];
+let mySecondAnswers = [];
+let myCompliteAnswers = [];
+
+function quizLoad(road) {
+  deselectAnswers();
+  questionEl.forEach((item) => {
+    item.innerHTML = road["quiz"][i].question;
+  });
+
+  a_text.innerHTML = road["quiz"][i].a;
+  b_text.innerHTML = road["quiz"][i].b;
+  c_text.innerHTML = road["quiz"][i].c;
+  d_text.innerHTML = road["quiz"][i].d;
+
+  NumOfQuestions();
+}
+
+let lengthOfQuiz;
+function NumOfQuestions() {
+  lengthOfQuiz =
+    ourRoad["quiz"].length + ourRoad["quiz2"].length + ourRoad["quiz3"].length;
+
+  numOfQuestions.forEach((item) => {
+    item.innerHTML = questionNum;
+  });
+  numOfAllQuestions.forEach((item) => {
+    item.innerHTML = lengthOfQuiz;
+  });
+}
+
+// openTest
+let ourRoad; //save choseed
+function openTest(road) {
+  main.classList.add("off");
+  ourRoad = quizDataAll[CHOSEED][road];
+  quizLoad(quizDataAll[CHOSEED][road]);
+  quiz.classList.add("quiz-container-show");
+}
+
+// selection validation
+function getSelected() {
+  let answer = null;
+  answerEls.forEach((item) => {
+    if (item.checked) {
+      answer = item.id;
+    }
+  });
+
+  return answer;
+}
+
+// delete chose  after send
+function deselectAnswers() {
+  answerEls.forEach((item) => {
+    item.checked = false;
+  });
+}
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const answer = getSelected();
+
+  // quizLoad(ourRoad);
+  if (answer) {
+    myAnswers.push(answer);
+    if (answer === ourRoad["quiz"][i].correct) {
+      score++;
+    }
+    i++;
+    currentQuiz++;
+    questionNum++;
+    if (currentQuiz < ourRoad["quiz"].length) {
+      quizLoad(ourRoad);
+    } else {
+      i = 0;
+      currentQuiz = 0;
+      nextQuiz();
+    }
+  } else {
+    alert("chose one of them!");
+  }
+});
+
+// reload
+function reset() {
+  window.location.reload();
+}
+
+// open sideNav
+function openSideNav() {
+  nav.classList.toggle("change");
+  setTimeout(() => {
+    sideNav.classList.toggle("active");
+  }, 200);
+}
+
+//open the new quiz
+function nextQuiz() {
+  quiz.classList.add("off");
+  quizInput.classList.remove("off");
+
+  nextQuizLoad();
+}
+
+let answerQuiz;
+input.addEventListener("input", (e) => {
+  answerQuiz = e.target.value.toLowerCase();
+  answerQuiz = answerQuiz.trim();
+});
+
+submitBtnInput.addEventListener("click", () => {
+  if (answerQuiz) {
+    mySecondAnswers.push(answerQuiz);
+    if (answerQuiz === ourRoad["quiz2"][i].correct) {
+      score++;
+    }
+    i++;
+    currentQuiz++;
+    questionNum++;
+    input.value = "";
+
+    if (currentQuiz < ourRoad["quiz2"].length) {
+      nextQuizLoad();
+    } else {
+      i = 0;
+      currentQuiz = 0;
+      openLastQuiz();
+    }
+  } else {
+    alert("complite this");
+  }
+});
+
+function nextQuizLoad() {
+  inputText.innerHTML = ourRoad["quiz2"][i].word;
+  questionEl.forEach((item) => {
+    item.innerHTML = ourRoad["quiz2"][i].question;
+  });
+  NumOfQuestions();
+}
+
+// open last quiz
+
+let answerQuizCompilte;
+compliteInput.addEventListener("input", (e) => {
+  answerQuizCompilte = e.target.value.toLowerCase();
+  answerQuizCompilte = answerQuizCompilte.trim();
+});
+
+function openLastQuiz() {
+  quizInput.classList.add("off");
+  quizComplite.classList.remove("off");
+
+  loadLastQuiz();
+}
+
+submitBtnComplite.addEventListener("click", () => {
+  if (answerQuizCompilte) {
+    myCompliteAnswers.push(answerQuizCompilte);
+    if (answerQuizCompilte === ourRoad["quiz3"][i].correct) {
+      score++;
+    }
+    i++;
+    currentQuiz++;
+    questionNum++;
+    compliteInput.value = "";
+    if (currentQuiz < ourRoad["quiz3"].length) {
+      loadLastQuiz();
+    } else {
+      quizComplite.innerHTML = `
+        <div class="quiz-over-modal">
+        <div class="content">
+            <h1>Хороший результат !</h1>
+            <p>Ты ответил на 
+                ${score} из 
+                ${lengthOfQuiz} вопросов
+            </p>
+            <button onclick="showMeRightAnswers()" id="btn-try-again">Показать результаты</button>
+        </div>
+    </div>
+        `;
+    }
+  } else {
+    alert("complite this");
+  }
+});
+
+function loadLastQuiz() {
+  inputTextFirst.innerHTML = ourRoad["quiz3"][i].first;
+  inputTextSecond.innerHTML = ourRoad["quiz3"][i].second;
+
+  questionEl.forEach((item) => {
+    item.innerHTML = ourRoad["quiz3"][i].question;
+  });
+  NumOfQuestions();
+}
+
+function showMeRightAnswers() {
+  
+  const templateFirst = quizDataAll[CHOSEED].map((item, index) => {
+    return `
+     <ol type="a" id="ol">
+      <h1 h1>${item["quiz"][index].question}</h1>
+      <li>${item["quiz"][index].a}</li>
+      <li>${item["quiz"][index].b}</li>
+      <li>${item["quiz"][index].c}</li>
+      <li>${item["quiz"][index].d}</li>
+      <h2>Правильный ответ: <span>${item["quiz"][index].correct}</span></h2>
+      <h2>Вы ответили: <span style="color: #f76767">${myAnswers[index]}</span></h2>
+      </ol>
+
+      <div id="answers-tracker"></div>
+    `;
+  }).join("");
+
+  const templateSecond = quizDataAll[CHOSEED].map((item, index) => {
+    return `
+    <ol type="a" id="ol">
+    <h1>${item["quiz2"][index].question}</h1>
+    <h2>Слово: <span>${item["quiz2"][index].word}</span></h2>
+    <h2>Правильный ответ: <span>${item["quiz2"][index].correct}</span></h2>
+    <h2>Вы ответили: <span style="color: #f76767">${mySecondAnswers[index]}</span></h2>
+    </ol>
+
+    <div id="answers-tracker"></div>
+    `;
+  }).join("");
+
+  const templateLast = quizDataAll[CHOSEED].map((item, index) => {
+    return `
+    <ol type="a" id="ol">
+    <h1>${item["quiz3"][index].question}</h1>
+    <h2>Правильный ответ: <span>${item["quiz3"][index].correct}</span></h2>
+    <h4>
+    <span>${item["quiz3"][index].first} </span>
+    <span style="color: #f76767"> ${myCompliteAnswers[index]} </span>
+    <span> ${item["quiz3"][index].second} </span>
+     </h4>
+     <h2>Вы ответили: <span style="color: #f76767">${myCompliteAnswers[index]}</span></h2>
+     </ol>
+
+    <div id="answers-tracker"></div>
+    `;
+  }).join("");
+
+  quizComplite.innerHTML = templateFirst;
+  quizComplite.insertAdjacentHTML("beforeend", templateSecond);
+  quizComplite.insertAdjacentHTML("beforeend", templateLast);
+  quizComplite.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div style="text-align: center;">
+    <h2>Вы ответили на ${score} из ${lengthOfQuiz} вопросов</h2>
+    <button id="btn-end" onclick="reset()">Завершить</button>
+    </div>
+  `
+  );
+}
